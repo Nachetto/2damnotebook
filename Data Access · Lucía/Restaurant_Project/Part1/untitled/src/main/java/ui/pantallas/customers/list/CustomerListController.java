19da5547ccb6ab11ab11cadbd6843a5b10bbcab2
Nachetto <115470440+Nachetto@users.noreply.github.com>
2023-09-25 13:06:56 +0200

    MainSubjectsSubits

---
+++ Data Access · Lucía/Restaurant_Project/Part1/untitled/src/main/java/ui/pantallas/customers/list/CustomerListController.java
@@ -0,0 +1,43 @@
+package ui.pantallas.customers.list;
+
+import jakarta.inject.Inject;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import model.Customer;
+import service.CustomerService;
+import ui.pantallas.common.BasePantallaController;
+import ui.pantallas.customers.common.CustomerCommon;
+
+import java.time.LocalDate;
+
+public class CustomerListController extends BasePantallaController {
+    @Inject
+    private CustomerCommon common;
+    @FXML
+    private TableView<Customer> customerlist;
+    @FXML
+    private TableColumn<Customer, Integer> customerphone;
+    @FXML
+    private TableColumn<Customer, String> customername;
+    @FXML
+    private TableColumn<Customer, String> customersurname;
+    @FXML
+    private TableColumn<Customer, String> customeremail;
+    @FXML
+    private TableColumn<Customer, LocalDate> customerbirthdate;
+    @FXML
+    private TableColumn<Customer, Integer> customerid;
+
+    @Inject
+    private CustomerService service;
+
+    public void initialize() {
+        common.initializeCustomerTable(customerid, customerphone, customerbirthdate, customeremail, customersurname, customername);
+    }
+
+    @Override
+    public void principalCargado() {
+        customerlist.getItems().addAll(service.getAll().get());
+    }
+}
---
+++ Data Access · Lucía/Restaurant_Project/Part1/untitled/src/main/java/ui/pantallas/orders/common/OrderCommon.java
@@ -0,0 +1,16 @@
+package ui.pantallas.orders.common;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Order;
+
+import java.time.LocalDateTime;
+
+public class OrderCommon {
+    public void initializeCustomerTable(TableColumn<Order, Integer> orderid, TableColumn<Order, Integer> tableid, TableColumn<Order, Integer> customerid, TableColumn<Order, LocalDateTime> orderdate) {
+        orderid.setCellValueFactory(new PropertyValueFactory<>("orderid"));
+        tableid.setCellValueFactory(new PropertyValueFactory<>("tableid"));
+        customerid.setCellValueFactory(new PropertyValueFactory<>("customerid"));
+        orderdate.setCellValueFactory(new PropertyValueFactory<>("orderdate"));
+    }
+}
