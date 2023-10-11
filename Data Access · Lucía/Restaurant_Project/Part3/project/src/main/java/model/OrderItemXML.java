366b47c53282bbae519005f56caab3720a976d4e
Nachetto <115470440+Nachetto@users.noreply.github.com>
2023-10-11 10:12:11 +0200

    ahbda11/10 v1

---
+++ Data Access · Lucía/Restaurant_Project/Part3/project/src/main/java/model/OrderItemXML.java
@@ -0,0 +1,9 @@
+package model;
+
+import lombok.Data;
+
+@Data
+public class OrderItemXML {
+    private String menuItem;
+    private int quantity;
+}
---
+++ Data Access · Lucía/Restaurant_Project/Part3/project/src/main/java/model/OrdersXML.java
@@ -0,0 +1,7 @@
+package model;
+
+import java.util.List;
+
+public class OrdersXML {
+    private List<OrderXML> orderListXML;
+}
