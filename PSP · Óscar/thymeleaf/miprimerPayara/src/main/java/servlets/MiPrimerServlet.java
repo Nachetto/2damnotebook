be92f4815ff25888761810ba922ce7b781e14ec2
Nachetto <itsnachetto@gmail.com>
2023-10-29 19:55:58 +0100

    29/10 v1 psp inicio

---
+++ PSP · Óscar/thymeleaf/miprimerPayara/src/main/java/servlets/MiPrimerServlet.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet( name = "Servlet", value = "/Servlet")
+public class MiPrimerServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("""
+                <html>
+                    <head>
+                        <title>Mi primer servlet</title>
+                    </head>
+                    <body>
+                        <h1>Mi primer servlet</h1>
+                        <p>Este es mi primer servlet</p>
+                    </body>
+                </html>
+                
+                """ 
+        );
+    }
+}
